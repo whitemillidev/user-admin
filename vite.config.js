3e1969fde3328b155134957d0b3d52ac4dc64cf5
Ramil <Ramilmys@gmail.com>
2026-04-02 04:01:02 +0700

    Добавлены прокси и игнорирование

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
     watch: {
-      ignored: ["db.json"],
+      ignored: ["**/db/**"],
     },
   },
 });
