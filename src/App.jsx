dbc236942f8b3819be27d8c0cf186786a7adcd06
Ramil <Ramilmys@gmail.com>
2026-04-02 04:05:43 +0700

    Все компоненты импортированы

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,17 @@
-export default function App() {}
+import React from "react";
+import CreateUsersForm from "./components/CreateUsersForm";
+import UsersList from "./components/UsersList";
+import styles from "./styles/create-user-form.module.css";
+import useToggle from "./hooks/useToggle";
+
+export default function App() {
+  const { value, toggle } = useToggle(false);
+
+  return (
+    <div className={styles["app"]}>
+      <button onClick={toggle}>{value ? "Show Create User Form" : "Show Users List"}</button>
+
+      {value ? <UsersList /> : <CreateUsersForm />}
+    </div>
+  );
+}
