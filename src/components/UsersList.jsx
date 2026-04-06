c9ef37effbc8bac3206ff91f7c900dac47b54086
Ramil <Ramilmys@gmail.com>
2026-04-07 04:52:34 +0700

    Компонент с пользователями

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,26 +1,55 @@
 import React from "react";
-import useFetch from "../hooks/useFetch";
 import styles from "../styles/users-list.module.css";
+import useUsers from "../hooks/useUsers";
+import useRemoveUser from "../hooks/useRemoveUser";
+import ReloadIcon from "../icons/ReloadIcon";
+import GarbageIcon from "../icons/GarbageIcon";
+import useUpdateUser from "../hooks/useUpdateUser";
+import UserFIeldRow from "./UserFieldRow";
 
 export default function UsersList() {
-  const { data } = useFetch("api/users");
+  const { data = [], isLoading, error } = useUsers();
+  const { mutate: removeUser } = useRemoveUser();
+  const { mutate: updateUser } = useUpdateUser();
 
   return (
     <div className={styles["users-list-container"]}>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
       <h1 className={styles["users-list-title"]}>Users list</h1>
       <div className={styles["users-list"]}>
         {data.map((user) => (
-          <div key={user.id}>
-            <div className={styles["users-list-item"]}>
-              <div className={styles["users-list-item-name"]}>
-                Fullname: {user.firstName} {user.lastName}
+          <form
+            key={user.id}
+            onSubmit={(e) => {
+              e.preventDefault();
+
+              const formData = Object.fromEntries(new FormData(e.target));
+
+              updateUser({
+                id: user.id,
+                data: formData,
+              });
+            }}
+          >
+            <div>
+              <div className={styles["users-list-item"]}>
+                <button className={styles["users-list-item-edit"]} type="submit">
+                  <ReloadIcon />
+                </button>
+                <button type="button" className={styles["users-list-item-delete"]} onClick={() => removeUser(user.id)}>
+                  <GarbageIcon />
+                </button>
+                <UserFIeldRow label="Firstname:" name="firstName" type="text" defaultValue={user.firstName} />
+                <UserFIeldRow label="Lastname:" name="lastName" type="text" defaultValue={user.lastName} />
+                <UserFIeldRow label="Age:" name="age" type="number" defaultValue={user.age} />
+                <UserFIeldRow label="Gender:" name="gender" type="text" defaultValue={user.gender} />
+                <UserFIeldRow label="Email:" name="email" type="text" defaultValue={user.email} />
+                <UserFIeldRow label="Username:" name="username" type="text" defaultValue={user.username} />
+                <UserFIeldRow label="Password:" name="password" type="text" defaultValue={user.password} />
               </div>
-              <div className={styles["users-list-item-age"]}>Age: {user.age}</div>
-              <div className={styles["users-list-item-gender"]}>Gender: {user.gender}</div>
-              <div className={styles["users-list-item-username"]}>Username: {user.username}</div>
-              <div className={styles["users-list-item-email"]}>Email: {user.email}</div>
             </div>
-          </div>
+          </form>
         ))}
       </div>
     </div>
