c2b6af33834ecfb145f8a6de85f21494b92f1338
Ramil <Ramilmys@gmail.com>
2026-04-07 04:53:34 +0700

    Все компоненты импортированы

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import CreateUsersForm from "./components/CreateUsersForm";
 import UsersList from "./components/UsersList";
 import styles from "./styles/create-user-form.module.css";
 import useToggle from "./hooks/useToggle";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   const { value, toggle } = useToggle(false);
 
   return (
-    <div className={styles["app"]}>
-      <button onClick={toggle}>{value ? "Show Create User Form" : "Show Users List"}</button>
+    <QueryClientProvider client={queryClient}>
+      <div className={styles["app"]}>
+        <button onClick={toggle}>{value ? "Show Create User Form" : "Show Users List"}</button>
 
-      {value ? <UsersList /> : <CreateUsersForm />}
-    </div>
+        {/* {value ? <UsersList /> : <CreateUsersForm />} */}
+        <UsersList />
+        <br />
+        <hr />
+        <br />
+        <CreateUsersForm />
+      </div>
+    </QueryClientProvider>
   );
 }
