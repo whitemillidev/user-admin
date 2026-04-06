4cd801a70a69f18f2608efd0003a015905bfa218
Ramil <Ramilmys@gmail.com>
2026-04-07 04:52:06 +0700

    Форма для добавления пользователей

--- src/components/CreateUsersForm.jsx
+++ src/components/CreateUsersForm.jsx
@@ -5,17 +5,23 @@ import CalendarIcon from "../icons/CalendarIcon";
 import GenderIcon from "../icons/GenderIcon";
 import MailIcon from "../icons/MailIcon";
 import FormField from "./FormField";
+import PasswIcon from "../icons/PasswIcon";
+import useAddUser from "../hooks/useAddUser";
 
 export default function CreateUsersForm() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = Object.fromEntries(new FormData(e.target));
-    console.log(formData);
-  };
-
+  const { mutate: addUser } = useAddUser();
   return (
     <div className={styles["create-users-form-container"]}>
-      <form className={styles["create-users-form"]} onSubmit={handleSubmit}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addUser(Object.fromEntries(new FormData(e.target)));
+          e.target.reset();
+
+          
+        }}
+        className={styles["create-users-form"]}
+      >
         <h2 className={styles["create-users-form-title"]}>Create Users Form</h2>
 
         <div className={styles["create-users-form-input-fullname"]}>
@@ -25,8 +31,10 @@ export default function CreateUsersForm() {
 
         <FormField name="age" label="Age" Icon={CalendarIcon} type="number" placeholder="20" />
         <FormField name="gender" label="Gender" Icon={GenderIcon} type="text" placeholder="Male" />
-        <FormField name="username" label="Username" Icon={UserIcon} type="text" placeholder="nexus_4235" />
         <FormField name="email" label="Email" Icon={MailIcon} type="email" placeholder="qwerty@gmail.com" />
+        <FormField name="username" label="Username" Icon={UserIcon} type="text" placeholder="nexus_4235" />
+        <FormField name="password" label="Password" Icon={PasswIcon} type="password" placeholder="••••••••" />
+        <FormField name="password" label="Repeat password" Icon={PasswIcon} type="password" placeholder="••••••••" />
 
         <button className={styles["create-users-form-button"]} type="submit">
           Add user
