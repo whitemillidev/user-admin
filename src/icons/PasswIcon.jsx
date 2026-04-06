89858c5d3ea583ae58badd983789210cc54b9297
Ramil <Ramilmys@gmail.com>
2026-04-07 04:51:28 +0700

    Добавлены новые иконки

---
+++ src/icons/PasswIcon.jsx
@@ -0,0 +1,20 @@
+export default function PasswIcon({ className }) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="16" r="1" />
+      <rect x="3" y="10" width="18" height="12" rx="2" />
+      <path d="M7 10V7a5 5 0 0 1 10 0v3" />
+    </svg>
+  );
+}
