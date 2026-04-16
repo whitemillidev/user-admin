250e0f5d487de3037bb75e65f7334736731080df
Ramil <Ramilmys@gmail.com>
2026-04-17 04:43:50 +0700

    Форма для редактирования пользователей отредактирована

--- src/components/UpdateUsersForm.jsx
+++ src/components/UpdateUsersForm.jsx
@@ -1,46 +1,75 @@
-import React from "react";
-import styles from "../styles/create-user-form.module.css";
+import React, { use } from "react";
+import styles from "../styles/update-user-form.module.css";
 import UserIcon from "../icons/UserIcon";
 import CalendarIcon from "../icons/CalendarIcon";
 import GenderIcon from "../icons/GenderIcon";
 import MailIcon from "../icons/MailIcon";
-import FormField from "./FormField";
 import PasswIcon from "../icons/PasswIcon";
 import useUpdateUser from "../hooks/useUpdateUser";
+import { setIsWatched, setSelectedUser, useUsersStore } from "../store/users";
+import CloseIcon from "../icons/CloseIcon";
+import UpdateFormSelect from "./UpdateFormSelect";
+import RightsIcon from "../icons/RightsIcon";
+import UpdateFormField from "./UpdateFormField";
+import EyeIcon from "../icons/EyeIcon";
+import IncognitoIcon from "../icons/IncognitoIcon";
 
 export default function UpdateUsersForm() {
   const { mutate: updateUser } = useUpdateUser();
+  const selectedUser = useUsersStore((state) => state.selectedUser);
+  const isWatched = useUsersStore((state) => state.isWatched);
+  console.log(isWatched);
+
   return (
-    <div className={styles["create-users-form-container"]}>
+    <div className={styles["update-users-form-container"]}>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          updateUser(Object.fromEntries(new FormData(e.target)));
+          const formData = Object.fromEntries(new FormData(e.target));
           e.target.reset();
 
           updateUser({
-            id: user.id,
+            id: selectedUser.id,
             data: formData,
           });
+          setSelectedUser(null);
         }}
-        className={styles["create-users-form"]}
+        className={styles["update-users-form"]}
       >
-        <h2 className={styles["create-users-form-title"]}>Update Users Form</h2>
+        <button
+          type="button"
+          className={styles["update-users-form-close-btn"]}
+          onClick={() => {
+            setSelectedUser(null);
+            setIsWatched(false);
+          }}
+        >
+          <CloseIcon />
+        </button>
+        <h2 className={styles["update-users-form-title"]}>Update Users Form</h2>
 
-        <div className={styles["create-users-form-input-fullname"]}>
-          <FormField name="firstName" label="First Name" Icon={UserIcon} type="text" placeholder="Ivan" />
-          <FormField name="lastName" label="Last Name" Icon={UserIcon} type="text" placeholder="Harris" />
+        <div className={styles["update-users-form-input-fullname"]}>
+          <UpdateFormField name="firstName" label="First Name" Icon={UserIcon} type="text" placeholder="Ivan" defaultValue={selectedUser.firstName} />
+          <UpdateFormField name="lastName" label="Last Name" Icon={UserIcon} type="text" placeholder="Harris" defaultValue={selectedUser.lastName} />
         </div>
+        <UpdateFormField name="age" label="Age" Icon={CalendarIcon} type="number" placeholder="20" defaultValue={selectedUser.age} />
+        <UpdateFormField name="gender" label="Gender" Icon={GenderIcon} type="text" placeholder="Male" defaultValue={selectedUser.gender} />
+        <UpdateFormField name="email" label="Email" Icon={MailIcon} type="email" placeholder="qwerty@gmail.com" defaultValue={selectedUser.email} />
+        <UpdateFormSelect name="roleId" label="Role" Icon={RightsIcon} defaultValue={selectedUser.roleId} />
+        <UpdateFormField name="username" label="Username" Icon={UserIcon} type="text" placeholder="nexus_4235" defaultValue={selectedUser.username} />
+        <UpdateFormField
+          name="password"
+          label="Password"
+          Icon={PasswIcon}
+          RightIcon={isWatched ? IncognitoIcon : EyeIcon}
+          onClick={() => setIsWatched(!isWatched)}
+          type={isWatched ? "text" : "password"}
+          placeholder="••••••••"
+          defaultValue={selectedUser.password}
+        />
 
-        <FormField name="age" label="Age" Icon={CalendarIcon} type="number" placeholder="20" />
-        <FormField name="gender" label="Gender" Icon={GenderIcon} type="text" placeholder="Male" />
-        <FormField name="email" label="Email" Icon={MailIcon} type="email" placeholder="qwerty@gmail.com" />
-        <FormField name="username" label="Username" Icon={UserIcon} type="text" placeholder="nexus_4235" />
-        <FormField name="password" label="Password" Icon={PasswIcon} type="password" placeholder="••••••••" />
-        <FormField name="password" label="Repeat password" Icon={PasswIcon} type="password" placeholder="••••••••" />
-
-        <button className={styles["create-users-form-button"]} type="submit">
-          Add user
+        <button className={styles["update-users-form-button"]} type="submit">
+          Update user
         </button>
       </form>
     </div>
