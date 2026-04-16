54afc4283bac7c17c894dad18938cca095c90a29
Ramil <Ramilmys@gmail.com>
2026-04-17 04:44:04 +0700

    Все компоненты импортированы

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import CreateUsersForm from "./components/CreateUsersForm";
 import UsersList from "./components/UsersList";
-import styles from "./styles/create-user-form.module.css";
 import useToggle from "./hooks/useToggle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createPortal } from "react-dom";
 import UsersTable from "./components/UsersTable";
+import UpdateUsersForm from "./components/UpdateUsersForm";
+import { useUsersStore } from "./store/users";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   const { value, toggle } = useToggle(false);
+  const selectedUser = useUsersStore((state) => state.selectedUser);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className={styles["app"]}>
+      <div>
         <button onClick={toggle}>{value ? "Show Users Table" : "Show Create User Form"}</button>
-
         {value ? <CreateUsersForm /> : <UsersTable />}
-
-        {/* <UsersList /> */}
-        {/* <UsersTable />
-        <br />
-        <hr />
-        <br />
-        {createPortal(<CreateUsersForm />, document.body)} */}
+        {selectedUser && <div className="overlay" />}
+        {selectedUser && <UpdateUsersForm />}
       </div>
     </QueryClientProvider>
   );
