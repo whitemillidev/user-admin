8d38e766c558102837c37ee60a0ffc94227065dd
Ramil <Ramilmys@gmail.com>
2026-04-17 21:33:29 +0700

    Добавлен поиск для запроса пользователей

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,18 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
+import { useUsersStore } from "../store/users";
 
 export default function useUsers() {
+  const search = useUsersStore((state) => state.search);
   return useQuery({
-    queryKey: ["users"],
+    queryKey: ["users", search],
     queryFn: () =>
-      fetch(`api/users`)
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error("Invalid url");
-          }
-          return res.json();
-        })
-        .catch((err) => {
-          console.error(err.message);
-        }),
+      fetch(`api/users?fullName:contains=${search}`).then((res) => {
+        if (!res.ok) {
+          throw new Error("Invalid url");
+        }
+        return res.json();
+      }),
   });
 }
