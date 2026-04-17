602fd69b11c4e68ec64e4b4a7ca3177b6f139dc7
Ramil <Ramilmys@gmail.com>
2026-04-17 21:33:40 +0700

    Добавлены новые стейты

--- src/store/users.js
+++ src/store/users.js
@@ -4,6 +4,9 @@ export const useUsersStore = create(() => ({
   selectedUser: null,
   roles: [],
   isWatched: false,
+  search: "",
+  firstName: "",
+  lastName: "",
 }));
 
 export function setSelectedUser(selectedUser) {
@@ -16,4 +19,16 @@ export function setRoles(roles) {
 
 export function setIsWatched(isWatched) {
   useUsersStore.setState({ isWatched });
+}
+
+export function setSearch(search) {
+  useUsersStore.setState({ search });
+}
+
+export function setFirstName(firstName) {
+  useUsersStore.setState({ firstName });
+}
+
+export function setLastName(lastName) {
+  useUsersStore.setState({ lastName });
 }
