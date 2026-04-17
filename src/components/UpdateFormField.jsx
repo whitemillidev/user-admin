9f68d86e95b73e55024a6f2d61cae06fd9bff6b7
Ramil <Ramilmys@gmail.com>
2026-04-17 21:37:09 +0700

    Убраны лишние пропсы

--- src/components/UpdateFormField.jsx
+++ src/components/UpdateFormField.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import styles from "../styles/update-user-form.module.css";
 
-export default function UpdateFormField({ className, name, label, Icon, RightIcon, onClick, defaultValue, ...props }) {
+export default function UpdateFormField({ className, name, label, Icon, RightIcon, onChange, onClick, value, defaultValue, ...props }) {
   return (
     <div className={styles["update-users-form-input-container"]}>
       <p className={styles["update-users-form-input-label"]}>{label}</p>
       <div className={styles["update-users-form-input-wrapper"]}>
         {Icon && <Icon className={styles["user-icon"]} />}
-        <input name={name} className={styles[`update-users-form-input-${name.toLowerCase()}`]} defaultValue={defaultValue} {...props} />
+        <input
+          name={name}
+          onChange={onChange}
+          className={styles[`update-users-form-input-${name.toLowerCase()}`]}
+          defaultValue={defaultValue}
+          value={value}
+          {...props}
+        />
         {RightIcon && (
           <button type="button" onClick={onClick} className={styles["user-icon-right-btn"]}>
             <RightIcon className={styles["user-icon-right"]} />
