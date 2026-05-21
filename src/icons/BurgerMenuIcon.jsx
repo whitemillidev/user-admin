e1916b7461e605a46f65fddabfefa2125d10ba1c
Ramil <Ramilmys@gmail.com>
2026-05-21 17:45:06 +0700

    Добавлена новая иконка

---
+++ src/icons/BurgerMenuIcon.jsx
@@ -0,0 +1,20 @@
+export default function BurgerMenuIcon({ className }) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M3 5h18" />
+      <path d="M3 12h18" />
+      <path d="M3 19h18" />
+    </svg>
+  );
+}
