f1d67e53e42ea0301b93988d31765a25a016b050
Ramil <Ramilmys@gmail.com>
2026-05-21 17:42:15 +0700

    Добавлен плагин для tanstackRouter

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
   server: {
     proxy: {
       "/api": {
