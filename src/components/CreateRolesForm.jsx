2d1203f7c54a151045e80452a1f1fb80f3dc1093
Ramil <Ramilmys@gmail.com>
2026-05-22 22:41:30 +0700

    Компонент с формой для создания ролей

---
+++ src/components/CreateRolesForm.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "../styles/create-user-form.module.css";
+import CreateFormSelect from "./CreateFormSelect";
+import RightsIcon from "../icons/RightsIcon";
+import { useShallow } from "zustand/shallow";
+import useAddRoles from "../hooks/useAddRoles";
+import CreateFormCheckbox from "./CreateFormCheckbox";
+import EyeIcon from "../icons/EyeIcon";
+import EditIcon from "../icons/EditIcon";
+import GarbageIcon from "../icons/GarbageIcon";
+import CreateFormField from "./CreateFormField";
+
+export default function CreateRolesForm() {
+  const { mutate: addRoles } = useAddRoles();
+  return (
+    <div className={styles["create-users-form-container"]}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          const formData = new FormData(e.target);
+
+          e.target.reset();
+
+          const roleData = {
+            roleName: formData.get("roleName"),
+            accesses: formData.getAll("accesses"),
+          };
+
+          addRoles(roleData);
+        }}
+        className={styles["create-users-form"]}
+      >
+        <h2 className={styles["create-users-form-title"]}>Create Roles Form</h2>
+        <CreateFormField name="roleName" label="Role" Icon={RightsIcon} type="text" placeholder="User" />
+
+        <CreateFormCheckbox
+          name="accesses"
+          label="Access rights"
+          Icon={RightsIcon}
+          options={["create", "read", "update", "delete"]}
+          OptionIcons={{
+            create: EditIcon,
+            read: EyeIcon,
+            update: EditIcon,
+            delete: GarbageIcon,
+          }}
+        />
+
+        <button className={styles["create-users-form-button"]} type="submit">
+          Add roles
+        </button>
+      </form>
+    </div>
+  );
+}
