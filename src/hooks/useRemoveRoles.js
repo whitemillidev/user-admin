a17c8a800d873fd637ba233b94ff4c0b041f449d
Ramil <Ramilmys@gmail.com>
2026-05-22 22:22:45 +0700

    Хук для удаления ролей

---
+++ src/hooks/useRemoveRoles.js
@@ -0,0 +1,14 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export default function useRemoveRoles() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (id) =>
+      fetch(`api/roles/${id}`, {
+        method: "DELETE",
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["roles"] });
+    },
+  });
+}
