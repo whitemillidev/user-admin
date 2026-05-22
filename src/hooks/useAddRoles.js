72ffbe69bbb02c78aaac68956542e692e8c71e33
Ramil <Ramilmys@gmail.com>
2026-05-22 22:22:33 +0700

    Хук для добавления ролей

---
+++ src/hooks/useAddRoles.js
@@ -0,0 +1,16 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export default function useAddRoles() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data) =>
+      fetch(`api/roles`, {
+        body: JSON.stringify(data),
+        method: "POST",
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["roles"] });
+    },
+  });
+}
