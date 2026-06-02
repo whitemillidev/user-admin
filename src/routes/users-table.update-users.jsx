418b5a18ed4829dcea0634c730e48e1d1557b1d9
Ramil <Ramilmys@gmail.com>
2026-06-03 01:34:56 +0700

    Добавлен вложенный маршрут для обновления пользовательских данных

---
+++ src/routes/users-table.update-users.jsx
@@ -0,0 +1,18 @@
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import UpdateUsersForm from "../components/UpdateUsersForm";
+
+export const Route = createFileRoute("/users-table/update-users")({
+  component: UpdateUsersPage,
+});
+
+function UpdateUsersPage() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="overlay" onClick={() => navigate({ to: "/users-table" })} />
+
+      <UpdateUsersForm />
+    </>
+  );
+}
