883d74a50ef6a0d8e9f37f020ad1dc875f21dfc1
Ramil <Ramilmys@gmail.com>
2026-06-03 01:33:24 +0700

    Добавлен вложенный маршрут для создания пользователей

---
+++ src/routes/users-table.add-users.jsx
@@ -0,0 +1,17 @@
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import CreateUsersForm from "../components/CreateUsersForm";
+
+export const Route = createFileRoute("/users-table/add-users")({
+  component: AddUsersPage,
+});
+
+function AddUsersPage() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="overlay" onClick={() => navigate({ to: "/users-table" })} />
+      <CreateUsersForm />
+    </>
+  );
+}
