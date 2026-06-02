7ad0979b196dbba02013c542c072f304933710e3
Ramil <Ramilmys@gmail.com>
2026-06-03 01:24:49 +0700

    Переписана поисковая система с zustand на хуки routing

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,15 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { useUsersStore } from "../store/users";
+import { useSearch } from "@tanstack/react-router";
 
 export default function useUsers() {
-  const search = useUsersStore((state) => state.searchUsers);
+  const { query = "" } = useSearch({ from: "/users-table" });
+
   return useQuery({
-    queryKey: ["users", search],
+    queryKey: ["users", query],
     queryFn: () =>
-      fetch(`/api/users?fullName:contains=${search}`).then((res) => {
-        if (!res.ok) {
-          throw new Error("Invalid url");
-        }
+      fetch(`/api/users?fullName:contains=${query}`).then((res) => {
+        if (!res.ok) throw new Error("Invalid url");
         return res.json();
       }),
   });
