6773924f2f31f0723ca6a12b3769dd801d94db36
Ramil <Ramilmys@gmail.com>
2026-06-03 01:23:39 +0700

    Переписаны селекты

--- src/components/RolesTable.jsx
+++ src/components/RolesTable.jsx
@@ -7,17 +7,19 @@ import GarbageIcon from "../icons/GarbageIcon";
 import useUpdateUser from "../hooks/useUpdateUser";
 import UserFIeldRow from "./UserFieldRow";
 import EditIcon from "../icons/EditIcon";
-import { setFirstName, setLastName, setSearch, setSelectedUser, useUsersStore } from "../store/users";
+import { setFirstName, setLastName, setSearchRoles, setSelectedUser, useUsersStore } from "../store/users";
 import useRoles from "../hooks/useRoles";
 import { useShallow } from "zustand/shallow";
 import CreateFormField from "./CreateFormField";
 import CalendarIcon from "../icons/CalendarIcon";
-import UserFilters from "./UserFilters";
+import UserFilters from "./DataFilters";
 import SearchIcon from "../icons/SearchIcon";
 import useRemoveRoles from "../hooks/useRemoveRoles";
+import { Link } from "@tanstack/react-router";
+import DataFilters from "./DataFilters";
 
 export default function RolesTable() {
-  const [selectedUser, search] = useUsersStore(useShallow((state) => [state.selectedUser, state.search]));
+  const [selectedUser, search] = useUsersStore(useShallow((state) => [state.selectedUser, state.searchRoles]));
 
   const { data = [], isLoading, error } = useRoles();
   const { mutate: removeRoles } = useRemoveRoles();
@@ -28,7 +30,12 @@ export default function RolesTable() {
       {isLoading && <div>Loading...</div>}
       {error && <div>Error: {error.message}</div>}
       <h1 className={styles["users-table-title"]}>Roles table</h1>
-      <UserFilters onChange={(e) => setSearch(e.target.value)} Icon={SearchIcon} placeholder={"Enter the user's role..."} />
+      <div className={styles["users-table-controls"]}>
+        <DataFilters onChange={(e) => setSearchRoles(e.target.value)} Icon={SearchIcon} placeholder={"Enter the role name..."} />
+        <Link to="/roles-table/add-roles">
+          <button className={styles["add-user-btn"]}>Add role</button>
+        </Link>
+      </div>
       <table className={styles["users-table"]}>
         <thead>
           <tr>
--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -7,28 +7,47 @@ import GarbageIcon from "../icons/GarbageIcon";
 import useUpdateUser from "../hooks/useUpdateUser";
 import UserFIeldRow from "./UserFieldRow";
 import EditIcon from "../icons/EditIcon";
-import { setFirstName, setLastName, setSearch, setSelectedUser, useUsersStore } from "../store/users";
+import { setFirstName, setLastName, setSearchUsers, setSelectedUser, useUsersStore } from "../store/users";
 import useRoles from "../hooks/useRoles";
 import { useShallow } from "zustand/shallow";
 import CreateFormField from "./CreateFormField";
 import CalendarIcon from "../icons/CalendarIcon";
 import SearchIcon from "../icons/SearchIcon";
-import DataFilters from "./UserFilters";
+import DataFilters from "./DataFilters";
+import { Link, useNavigate, useSearch } from "@tanstack/react-router";
 
 export default function UsersTable() {
-  const [selectedUser, search] = useUsersStore(useShallow((state) => [state.selectedUser, state.search]));
-
+  const [selectedUser] = useUsersStore(useShallow((state) => [state.selectedUser, state.searchUser]));
   const { data = [], isLoading, error } = useUsers();
   const { data: roles = [] } = useRoles();
   const { mutate: removeUser } = useRemoveUser();
   const { mutate: updateUser } = useUpdateUser();
+  const { query = "" } = useSearch({ from: "/users-table" });
+  const navigate = useNavigate();
 
   return (
     <div className={styles["users-table-container"]}>
       {isLoading && <div>Loading...</div>}
       {error && <div>Error: {error.message}</div>}
       <h1 className={styles["users-table-title"]}>Users table</h1>
-      <DataFilters onChange={(e) => setSearch(e.target.value)} Icon={SearchIcon} placeholder={"Enter the user's name..."} />
+      <div className={styles["users-table-controls"]}>
+        <DataFilters
+          value={query}
+          onChange={(e) => {
+            const value = e.target.value;
+            navigate({
+              search: value ? { query: value } : {},
+              replace: true,
+            });
+          }}
+          Icon={SearchIcon}
+          placeholder={"Enter the user's name..."}
+        />
+
+        <Link to="/users-table/add-users">
+          <button className={styles["add-user-btn"]}>Add user</button>
+        </Link>
+      </div>
       <table className={styles["users-table"]}>
         <thead>
           <tr>
@@ -46,35 +65,39 @@ export default function UsersTable() {
         </thead>
 
         <tbody>
-          {data.map((user) => (
-            <tr key={user.id} className={styles["users-table-row"]}>
-              <td>{user.firstName}</td>
-              <td>{user.lastName}</td>
-              <td>{user.gender}</td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>{roles.find((role) => role.id === user.roleId)?.roleName}</td>
-              <td>{user.password}</td>
-              <td>{user.age}</td>
+          {data.map((user) => {
+            return (
+              <tr key={user.id} className={styles["users-table-row"]}>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.gender}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{roles.find((role) => role.id === user.roleId)?.roleName}</td>
+                <td>{user.password}</td>
+                <td>{user.age}</td>
 
-              <td>
-                <button
-                  className={styles["users-table-item-edit"]}
-                  onClick={() => {
-                    setSelectedUser(user);
-                  }}
-                >
-                  <EditIcon />
-                </button>
-              </td>
+                <td>
+                  <Link to="/users-table/update-users">
+                    <button
+                      className={styles["users-table-item-edit"]}
+                      onClick={() => {
+                        setSelectedUser(user);
+                      }}
+                    >
+                      <EditIcon />
+                    </button>
+                  </Link>
+                </td>
 
-              <td>
-                <button className={styles["users-table-item-delete"]} onClick={() => removeUser(user.id)}>
-                  <GarbageIcon />
-                </button>
-              </td>
-            </tr>
-          ))}
+                <td>
+                  <button className={styles["users-table-item-delete"]} onClick={() => removeUser(user.id)}>
+                    <GarbageIcon />
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
