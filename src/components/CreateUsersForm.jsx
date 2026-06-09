054706202fa44882c2c8fbf99bdcacb2817832e7
Ramil <Ramilmys@gmail.com>
2026-06-10 03:03:00 +0700

    Подправлена логика для данных формы, добавлено всплывающее окно при закрытии формы

--- src/components/CreateUsersForm.jsx
+++ src/components/CreateUsersForm.jsx
@@ -1,26 +1,53 @@
 import React from "react";
+
+import { useShallow } from "zustand/shallow";
+import { useNavigate } from "@tanstack/react-router";
+
 import styles from "../styles/create-user-form.module.css";
 import UserIcon from "../icons/UserIcon";
 import CalendarIcon from "../icons/CalendarIcon";
 import GenderIcon from "../icons/GenderIcon";
 import MailIcon from "../icons/MailIcon";
 import PasswIcon from "../icons/PasswIcon";
-import useAddUser from "../hooks/useAddUser";
-import { setFirstName, setIsWatched, setLastName, useUsersStore } from "../store/users";
-import CreateFormSelect from "./CreateFormSelect";
 import RightsIcon from "../icons/RightsIcon";
-import CreateFormField from "./CreateFormField";
 import EyeIcon from "../icons/EyeIcon";
 import IncognitoIcon from "../icons/IncognitoIcon";
-import { useShallow } from "zustand/shallow";
-import useRoles from "../hooks/useRoles";
-import useUsers from "../hooks/useUsers";
-import { useNavigate } from "@tanstack/react-router";
 import CloseIcon from "../icons/CloseIcon";
 
+import CreateFormSelect from "./CreateFormSelect";
+import CreateFormField from "./CreateFormField";
+
+import useRoles from "../hooks/useRoles";
+import useUsers from "../hooks/useUsers";
+import useAddUser from "../hooks/useAddUser";
+import {
+  resetCreateUserForm,
+  setAge,
+  setEmail,
+  setFirstName,
+  setGender,
+  setIsWatched,
+  setLastName,
+  setPassword,
+  setRoleId,
+  setUsername,
+  useUsersStore,
+} from "../store/users";
+
 export default function CreateUsersForm() {
-  const [selectedUser, isWatched, firstName, lastName] = useUsersStore(
-    useShallow((state) => [state.selectedUser, state.isWatched, state.firstName, state.lastName]),
+  const [selectedUser, isWatched, firstName, lastName, age, gender, roleId, email, username, password] = useUsersStore(
+    useShallow((state) => [
+      state.selectedUser,
+      state.isWatched,
+      state.firstName,
+      state.lastName,
+      state.age,
+      state.gender,
+      state.roleId,
+      state.email,
+      state.username,
+      state.password,
+    ]),
   );
   const { data: roles = [] } = useRoles();
   const { data: users = [] } = useUsers();
@@ -43,6 +70,16 @@ export default function CreateUsersForm() {
           setIsWatched(false);
           setFirstName("");
           setLastName("");
+          setAge("");
+          setGender("");
+          setRoleId("");
+          setEmail("");
+          setUsername("");
+          setPassword("");
+
+          navigate({
+            to: "/users-table",
+          });
         }}
         className={styles["create-users-form"]}
       >
@@ -50,6 +87,13 @@ export default function CreateUsersForm() {
           type="button"
           className={styles["create-users-form-close-btn"]}
           onClick={() => {
+            const confirmed = window.confirm("Вы действительно хотите закрыть окно? Введенные вами данные будут утеряны.");
+
+            if (!confirmed) return;
+
+            resetCreateUserForm();
+            setIsWatched(false);
+
             navigate({
               to: "/users-table",
             });
@@ -62,6 +106,7 @@ export default function CreateUsersForm() {
         <div className={styles["create-users-form-input-fullname-container"]}>
           <CreateFormField
             name="firstName"
+            value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
             label="First Name"
             Icon={UserIcon}
@@ -70,6 +115,7 @@ export default function CreateUsersForm() {
           />
           <CreateFormField
             name="lastName"
+            value={lastName}
             onChange={(e) => setLastName(e.target.value)}
             label="Last Name"
             Icon={UserIcon}
@@ -78,9 +124,19 @@ export default function CreateUsersForm() {
           />
         </div>
 
-        <CreateFormField name="age" label="Age" Icon={CalendarIcon} type="number" placeholder="20" />
+        <CreateFormField
+          name="age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          label="Age"
+          Icon={CalendarIcon}
+          type="number"
+          placeholder="20"
+        />
         <CreateFormSelect
           name="gender"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
           label="Gender"
           Icon={GenderIcon}
           options={[
@@ -90,6 +146,8 @@ export default function CreateUsersForm() {
         />
         <CreateFormSelect
           name="roleId"
+          value={roleId}
+          onChange={(e) => setRoleId(e.target.value)}
           label="Role"
           Icon={RightsIcon}
           options={roles.map((role) => ({
@@ -97,10 +155,28 @@ export default function CreateUsersForm() {
             label: role.roleName,
           }))}
         />
-        <CreateFormField name="email" label="Email" Icon={MailIcon} type="email" placeholder="qwerty@gmail.com" />
-        <CreateFormField name="username" label="Username" Icon={UserIcon} type="text" placeholder="nexus_4235" />
+        <CreateFormField
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          label="Email"
+          Icon={MailIcon}
+          type="email"
+          placeholder="qwerty@gmail.com"
+        />
+        <CreateFormField
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          label="Username"
+          Icon={UserIcon}
+          type="text"
+          placeholder="nexus_4235"
+        />
         <CreateFormField
           name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           label="Password"
           Icon={PasswIcon}
           RightIcon={isWatched ? IncognitoIcon : EyeIcon}
@@ -110,6 +186,8 @@ export default function CreateUsersForm() {
         />
         <CreateFormField
           name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           label="Password"
           Icon={PasswIcon}
           RightIcon={isWatched ? IncognitoIcon : EyeIcon}
