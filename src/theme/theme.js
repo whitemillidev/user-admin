c3c218939e436b9545e4e95a2b12804e9837d7d4
Ramil <Ramilmys@gmail.com>
2026-08-06 05:02:28 +0700

    Файл стилей мантин отредактирован

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,4 +1,4 @@
-import { Button, createTheme } from "@mantine/core";
+import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
   components: {
@@ -6,36 +6,10 @@ export const theme = createTheme({
       defaultProps: {
         radius: "md",
       },
-
-      styles: {
-        input: {
-          backgroundColor: "rgb(20, 20, 30)",
-          color: "rgb(203, 202, 202)",
-          border: "1px solid rgb(63, 63, 63)",
-        },
-
-        label: {
-          color: "rgb(203, 202, 202)",
-        },
-      },
     },
-
     Select: {
-      styles: {
-        input: {
-          backgroundColor: "rgb(20, 20, 30)",
-          border: "1px solid rgb(63, 63, 63)",
-          color: "rgb(203, 202, 202)",
-        },
-        label: {
-          color: "rgb(203, 202, 202)",
-        },
-        dropdown: {
-          backgroundColor: "rgb(20, 20, 30)",
-        },
-        option: {
-          color: "rgb(203, 202, 202)",
-        },
+      defaultProps: {
+        radius: "md",
       },
     },
   },
